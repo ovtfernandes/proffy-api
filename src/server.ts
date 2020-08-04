be9f1242f5edb6b910d89d2336fc13c4330bafc0
Vitor Fernandes <s.fernandes.vitor@gmail.com>
2020-08-04 11:49:21 -0300

    Adds dotenv dependency

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,14 @@
-console.log('Hello World');
+import dotenv from 'dotenv';
+dotenv.config();
+
+import express from 'express';
+
+const app = express();
+
+app.get('/users', (req, res) => {
+    console.log('acessou a rota');
+});
+
+const PORT = process.env.PORT || 3333;
+
+app.listen(PORT);
