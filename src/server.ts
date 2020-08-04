cc5c71f4840ee0dd18863ff220dd0b18ee18d149
Vitor Fernandes <s.fernandes.vitor@gmail.com>
2020-08-04 14:07:12 -0300

    Adds cors

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import express from 'express';
+import cors from 'cors';
 import routes from './routes';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
